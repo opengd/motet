5e08c5402fab69733bb15a216e7a1c125552ea15
Erik Johansson <opengd@outlook.com>
2018-06-15 16:08:20 +0200

    code clean

--- motet.js
+++ motet.js
@@ -27,20 +27,20 @@ function extCheck(filepath) {
 
 function initStats() {
     db.set('stats.songs', db.get('music')
-    .size()
-    .value()).write();
+        .size()
+        .value()).write();
 
     db.set('stats.artist', db.get('music')
-    .map('artist')
-    .uniq()
-    .size()
-    .value()).write();
+        .map('artist')
+        .uniq()
+        .size()
+        .value()).write();
 
     db.set('stats.albums', db.get('music')
-    .map('album')
-    .uniq()
-    .size()
-    .value()).write();
+        .map('album')
+        .uniq()
+        .size()
+        .value()).write();
 }
 
 function initStatus() {
@@ -56,8 +56,6 @@ function updateDatabase() {
     config = low(adapterConfig);
 
     var nmbMusicPaths = config.get('music_paths').size().value();
-    //console.log('nmbMusicPaths: ' + nmbMusicPaths);
-    //console.log("config.get('music_paths').value()" + config.get('music_paths').value());
 
     // Clear the music database, a quick fix to remove any loose entries on update.
     clearMusicDatabase();
@@ -65,7 +63,6 @@ function updateDatabase() {
     config.get('music_paths').value().forEach(searchpath => {
         const excludeDirFilter = through2.obj(function (item, enc, next) {
             if (!item.stats.isDirectory() && extCheck(item.path)) this.push(item);
-            //else console.log(item.path);
             next();
           });
 
@@ -77,61 +74,30 @@ function updateDatabase() {
                 .then(meta => {                
                     // Get the md5 for the meta information.
                     var md5 = getMetaHash(meta, item.path);
-                    
-                    // Get the post if it exist in the database already.
-                    //var post = isFileInDb(md5);
-                    
-                    //if(!post) {
-                        // Add the new file and meta to the database.
-                        addMusicFileToDb(item, meta, md5, timestamp);                    
-                        //console.log("OK: " + item.path)
-                    //} else {
-                        // If the filepath is not equal to the post already in database
-                        // then update the filepath to use the new one.
-                        //if(post.file != item.path) updateFilePath(md5, item.path);
 
-                        //updateDbTimestamp(md5, timestamp);
-                    //}
+                    // Add the new file and meta to the database.
+                    addMusicFileToDb(item, meta, md5, timestamp);                    
                 })
                 .catch(error => {
-                    //console.log("ERROR: " + item.path);
-
                     // Get the md5 for the meta information.
                     var md5 = hasha(item.path, {algorithm: 'md5'});
 
-                    // Get the post if it exist in the database already.
-                    //var post = isFileInDb(md5);
-
-                    //if(!post) {
-                        //console.log("ADD as Unknown: " + item.path);
-
-                        // Add as unknown Artist and Album to the database.
-                        addUnknowFileToDb(item.path, md5, timestamp);
-                    //} else {
-                        //updateDbTimestamp(md5, timestamp);
-                    //}
+                    // Add as unknown Artist and Album to the database.
+                    addUnknowFileToDb(item.path, md5, timestamp);
                 })        
             })
         .on('end', () => {
             
             nmbMusicPaths--;
             
-            if(nmbMusicPaths == 0) {
-                
-                //var lastTimestamp = db.get('status.db_update_timestamp').value();
-
-                //if(lastTimestamp) {
-                //    db.get('music').remove({db_timestamp: lastTimestamp}).write();
-                //}
-                
+            if(nmbMusicPaths == 0) {               
                 db.set('status.db_update', false).write();
                 db.set('status.db_update_timestamp', timestamp).write(); 
                 
                 // Refresh the stats database to reflect the update.
                 initStats();
                 console.log(getStats());
-                console.log("Database update finish in " + (Date.now() - timestamp)  + "Ms.");
-                
+                console.log("Database update finish in " + (Date.now() - timestamp)  + "Ms.");              
             }
         });
     });
@@ -173,33 +139,33 @@ app.get('/artists', (req, res) => {
 app.get('/artist/:artist', (req, res) => { 
     console.log(req.params.artist);
     console.log(db.get('music')
-    .filter({artist: req.params.artist})
-    .map('album')
-    .uniq()
-    .sort()
-    .value());
+        .filter({artist: req.params.artist})
+        .map('album')
+        .uniq()
+        .sort()
+        .value());
 
     res.json(db.get('music')
-    .filter({artist: req.params.artist})
-    .map('album')
-    .uniq()
-    .sort()
-    .value());
+        .filter({artist: req.params.artist})
+        .map('album')
+        .uniq()
+        .sort()
+        .value());
 });
 
 app.get('/artist/:artist/album/:album', (req, res) => {     
     res.json(db.get('music')
-    .filter({artist: req.params.artist, album: req.params.album})
-    .sortBy('track')
-    .value());
+        .filter({artist: req.params.artist, album: req.params.album})
+        .sortBy('track')
+        .value());
 });
 
 app.get('/albums', (req, res) => { 
     res.json(db.get('music')
-    .map('album')
-    .uniq()
-    .sort()
-    .value());
+        .map('album')
+        .uniq()
+        .sort()
+        .value());
 });
 
 app.get('/music/:md5', (req, res) => { 
@@ -216,9 +182,9 @@ app.get('/queue/:md5', (req, res) => {
     if(post) {
 
         var songs = db.get('music')
-        .filter({artist: post.artist, album: post.album})
-        .sortBy('track')
-        .value();
+            .filter({artist: post.artist, album: post.album})
+            .sortBy('track')
+            .value();
 
         console.log(songs.drop(songs.findIndex(o => o.md5 == req.params.md5)));
 
@@ -255,48 +221,48 @@ function isFileInDb(md5) {
 
 function addMusicFileToDb(item, meta, md5, timestamp) {
     db.get('music')
-    .push({
-        "md5": md5,  
-        "path": item.path, 
-        "artist": meta.common.artist ? meta.common.artist : meta.common.artists[0],
-        "title": meta.common.title,
-        "track": meta.common.track.no,
-        "album": meta.common.album,
-        "dataformat": meta.format.dataformat,
-        "bitrate": meta.format.bitrate,
-        "sampleRate": meta.format.sampleRate,
-        "numberOfChannels": meta.format.numberOfChannels,
-        "duration": meta.format.duration,
-        "db_timestamp": timestamp
-    })
-    .write();
+        .push({
+            "md5": md5,  
+            "path": item.path, 
+            "artist": meta.common.artist ? meta.common.artist : meta.common.artists[0],
+            "title": meta.common.title,
+            "track": meta.common.track.no,
+            "album": meta.common.album,
+            "dataformat": meta.format.dataformat,
+            "bitrate": meta.format.bitrate,
+            "sampleRate": meta.format.sampleRate,
+            "numberOfChannels": meta.format.numberOfChannels,
+            "duration": meta.format.duration,
+            "db_timestamp": timestamp
+        })
+        .write();
 }
 
 function addUnknowFileToDb(filepath, md5, timestamp) {
 
     var tracks = db.get('music')
-    .filter({artist: "Unknown", album: "Unknown"})
-    .size()
-    .value();
+        .filter({artist: "Unknown", album: "Unknown"})
+        .size()
+        .value();
 
     var dataformat = path.extname(filepath).slice(1);
 
     db.get('music')
-    .push({
-        "md5": md5,  
-        "path": filepath, 
-        "artist": "Unknown",
-        "title": filepath,
-        "track": tracks,
-        "album": "Unknown",
-        "dataformat": dataformat,
-        "bitrate": 0,
-        "sampleRate": 0,
-        "numberOfChannels": 0,
-        "duration": 0,
-        "db_timestamp": timestamp
-    })
-    .write();
+        .push({
+            "md5": md5,  
+            "path": filepath, 
+            "artist": "Unknown",
+            "title": filepath,
+            "track": tracks,
+            "album": "Unknown",
+            "dataformat": dataformat,
+            "bitrate": 0,
+            "sampleRate": 0,
+            "numberOfChannels": 0,
+            "duration": 0,
+            "db_timestamp": timestamp
+        })
+        .write();
 }
 
 function updateDbTimestamp(md5, timestamp) {
